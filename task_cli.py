48b7ddb64ae28c71151e5460a7559cdf8f0db239
hanif0526 <hanifabdurrahmanarifai@gmail.com>
2026-03-23 17:42:06 +0700

    update task_cli.py

--- task_cli.py
+++ task_cli.py
@@ -13,6 +13,44 @@ def load_tasks():
         try:
             return json.load(file)
         except:
-            return json.JSONDecodeError:
+            return json.JSONDecodeError
                 return []
                 
+#2. Helper : Save data to the JSON file
+def save_tasks():
+    with open(FILE_PATH, 'w') as file:
+        json.dump(tasks, file, indent=4)           
+        
+                          
+#3. Main logic
+def main():
+    # sys.argv collects command line arguments
+    # [script_name, command, first.arg, second.arg...]    
+    if len(sys.argv) < 2:
+        print("Usage python python task_cli.py [add | list | update | delete | mark-in-progress | mark-done]")
+        return
+        
+    command = sys.argv[1].lower()
+    tasks = load_tasks()
+    
+    if command == "add":
+        description = " ".join(sys.argv[2:])
+        new_task = {
+        "id": len(tasks) + 1,
+        "description": description,
+        "status": "todo",
+        "createdAt": datetime.now().isoformat(),
+        "updateAt": datime.now().isoformat()  
+        }
+        tasks.append(new_tasks)
+        save_tasks(tasks)
+        print(f"Task added successfuly (ID : {new_task['id']})")
+        
+     elif command == "list":
+     	status_filter = sys.argv[2] if len(sys.argv) > 2 else None
+     	for t in tasks:
+     	    if status_filter is None or t['status'] == status_filter:
+     	        print(f"[{t['id']}] {t['description']} - Status : {t['status']} ")
+          
+        
+                                                                                                                    
